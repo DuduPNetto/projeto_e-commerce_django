4fc820be4658fb3724e3864dcbf130dc238323f5
DuduPNetto <dudupnetto.en@gmail.com>
2023-06-23 12:15:06 -0300

    Creating Product and Variation models

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from product import models
+
+
+@admin.register(models.Variation)
+class Variation(admin.ModelAdmin):
+    list_display = ('id', 'product', 'name', 'price',
+                    'promotional_price', 'stock')
+    list_display_links = ('id', 'product', 'name')
+    search_fields = ('id', 'product', 'name')
+    ordering = ('-id',)
+    list_filter = ('id',)
+    list_editable = ('price', 'promotional_price', 'stock')
+
+
+class VariationInline(admin.TabularInline):
+    model = models.Variation
+    extra = 1
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'price', 'promotional_price')
+    list_display_links = ('id', 'name', 'slug')
+    search_fields = ('id', 'name', 'slug')
+    ordering = ('-id',)
+    list_filter = ('id',)
+    list_editable = ('price', 'promotional_price')
+    inlines = [VariationInline]
--- product/models.py
+++ product/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+from utils.resize_image import resize_image
+
+
+class Product(models.Model):
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    name = models.CharField(max_length=100)
+    short_description = models.TextField()
+    long_description = models.TextField()
+    image = models.ImageField(
+        upload_to='image_products/%Y/%m', blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    price = models.FloatField()
+    promotional_price = models.FloatField(default=0)
+    product_type = models.CharField(
+        default='V', max_length=1, choices=(('V', 'Variação'), ('S', 'Simples'))
+    )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        max_image_size = 800
+
+        if self.image:
+            resize_image(self.image, max_image_size)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Variation(models.Model):
+    class Meta:
+        verbose_name = 'Variation'
+        verbose_name_plural = 'Variations'
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    price = models.FloatField()
+    promotional_price = models.FloatField(default=0)
+    stock = models.PositiveIntegerField(default=1)
+
+    def __str__(self) -> str:
+        return self.name or self.product.name
